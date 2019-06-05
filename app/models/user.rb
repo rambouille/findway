a634d2bbe307ad32583ceb8dd05e5b8b6390be9a
Antoine Rambert <antoine.rambert@gmail.com>
2019-06-05 18:24:30 +0200

    filters on coaches - backend

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,7 +29,7 @@
 class User < ApplicationRecord
   SPECIALIZATIONS = ["reconversion professionnelle", "conseiller d'orientation", "coaching personnalisé"]
 
-  HOURLY_PRICE_CENTS = [0..20, 21..40, 41..60, 61..80, 81..100, 101..120]
+  # HOURLY_PRICE_CENTS = [0..20, 21..40, 41..60, 61..80, 81..100, 101..120]
 
   BUSINESS_EXPERTISES = [
     "AGRICULTURE - BOIS",
