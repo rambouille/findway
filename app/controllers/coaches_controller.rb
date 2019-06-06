52846205dd1d9ca2ffec7936f2a9728acbf7061e
Benoit Michelin <benoit.michelin@gmail.com>
2019-06-06 10:07:27 +0200

    debut show

--- app/controllers/coaches_controller.rb
+++ app/controllers/coaches_controller.rb
@@ -1,6 +1,10 @@
 class CoachesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
+  skip_before_action :authenticate_user!, only: [:index, :show ]
   def index
     @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 end
