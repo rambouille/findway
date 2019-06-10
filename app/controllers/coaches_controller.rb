9c8cce5639fd841175b5cad6fa95930ac2e8e358
Antoine Rambert <antoine.rambert@gmail.com>
2019-06-10 11:37:12 +0200

    create little card on the right with the next slot and a button book

--- app/controllers/coaches_controller.rb
+++ app/controllers/coaches_controller.rb
@@ -2,21 +2,14 @@ class CoachesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show ]
   def index
     @users = User.coach
-    if params[:speciality].present?
-      @users = @users.where(speciality: params[:speciality])
-    end
-    if params[:business_expertise].present?
-      @users = @users.where(business_expertise: params[:business_expertise])
-    end
-    if params[:price_min].present?
-      @users = @users.where("hourly_price_cents >= #{params[:price_min].to_i * 100}")
-    end
-    if params[:price_max].present?
-      @users = @users.where("hourly_price_cents <= #{params[:price_max].to_i * 100}")
-    end
+    @users = @users.where(speciality: params[:speciality]) if params[:speciality].present?
+    @users = @users.where(business_expertise: params[:business_expertise]) if params[:business_expertise].present?
+    @users = @users.where("hourly_price_cents >= #{params[:price_min].to_i * 100}") if params[:price_min].present?
+    @users = @users.where("hourly_price_cents <= #{params[:price_max].to_i * 100}") if params[:price_max].present?
   end
 
   def show
     @user = User.find(params[:id])
+    @next_slot = @user.coach_bookings.where("start_time > ?", Time.now).order(:start_time).first
   end
 end
