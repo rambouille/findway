c9321f03dd468f78369db59020df9fd1a6c2900b
Nicolas Chican <nchican@omacinema.com>
2019-06-13 18:40:54 +0200

    pointer_card_index

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,3 +16,11 @@ import { displayCalendarCoach } from "calendar/calendar_coach";
 // chat
 import { scrollLastMessageIntoView } from "chat/scroll";
 window.scrollLastMessageIntoView = scrollLastMessageIntoView;
+
+// change mouse cursor on card coach
+// const card = document.querySelector(".card-index");
+// card.addEventListener("mouseover", {
+//   cursor = "grab";
+// })
+
+document.querySelector(".card-index").style.cursor = "pointer";
