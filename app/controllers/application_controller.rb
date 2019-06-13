b4504f5627da17affe30a7d032c82ec88060d117
Benoit Michelin <benoit.michelin@gmail.com>
2019-06-13 10:22:11 +0200

    checklist

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
   # include Pundit
 
   # # Pundit: white-list approach.
