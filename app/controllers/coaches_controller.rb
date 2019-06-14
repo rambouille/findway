a9fa2f5bdeaa3ed1ed7da5bcc03aa8dd8992b441
Antoine Rambert <antoine.rambert@gmail.com>
2019-06-14 09:41:24 +0200

    Change location o constant DAYS and MONTHS

--- app/controllers/coaches_controller.rb
+++ app/controllers/coaches_controller.rb
@@ -40,6 +40,4 @@ class CoachesController < ApplicationController
     chatroom = ChatRoom.create name: "#{coach.firstname} & #{client.firstname}", coach: coach, client: client
     Message.create content: "Bonjour, merci pour votre réservation pour le #{booking.french_date}! Si vous avez des questions avant notre entretien je suis à votre disposition.", chat_room: chatroom, user: chatroom.coach
   end
-
 end
-
